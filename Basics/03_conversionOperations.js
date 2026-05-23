bba55d4d8b31aae2d4c057340257630205e0bc40
SakshiPatil06032005 <168762303+SakshiPatil06032005@users.noreply.github.com>
2026-05-23 11:27:47 +0530

    Add data types and conversion operations examples

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+let score = "41" 
+let marks = "41abc"
+console.log(typeof score)
+console.log(score)
+
+let inNumberScore = Number(score)  // It will convert the string to a number
+console.log(typeof inNumberScore)
+console.log(inNumberScore)
+
+let inNumerScore = Number(score)  // It will convert the string to a number
+console.log(typeof inNumerScore)
+console.log(inNumerScore)
+
+let score2 = null
+
+let inNumerScore2 = Number(score2)  // It will convert null to 0
+console.log(typeof inNumerScore2)
+console.log(inNumerScore2)  
+
+let score3 = undefined
+
+let inNumerScore3 = Number(score3)  // It will convert undefined to NaN
+console.log(typeof inNumerScore3)
+console.log(inNumerScore3)  
+
+/*
+"41" => 41
+"41abc" => NaN
+null => 0
+undefined => NaN
+*/
+
+let isLoggedIn = true
+console.log(Number(isLoggedIn))  // It will convert the string "true" to NaN
+
+let isLoggedIn2 = ""
+console.log(Number(isLoggedIn2))  // It will convert the string "false" to NaN  
+
+let isLoggedIn3 = "Sakshi"
+console.log(Number(isLoggedIn3))  // It will convert the string "Sakshi" to NaN
+
+/*
+true => 1
+false => 0
+"" => 0
+"Sakshi" => NaN     
+*/
+
+
+let val = 41
+let valInString = String(val)
+console.log(typeof valInString)
+
+let val2 = true
+let valInString2 = String(val2)
+console.log("Value from boolean to string : ",valInString2)
+console.log(typeof(valInString2))
+
+let isAuthorized = 1
+let isAuthInBool = Boolean(isAuthorized)
+console.log(isAuthInBool)
+
+// 0 => false
+// 1 => true
+// any number => true
