9c663a93a7a06f43e7452bc76c21746d55f493c0
SakshiPatil06032005 <168762303+SakshiPatil06032005@users.noreply.github.com>
2026-05-23 11:08:28 +0530

    Add initial JavaScript files for variables, data types, and hello world example

---
+++ Basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // It treats all js code as a newer version
+
+// alert(3 + 3)            We are using nodejs not the browser
+
+console.log(3 + 3)
+console.log("Sakshi")
+
+let name = "Sakshi"
+let age = 21
+let isLoggedIn = false
+
+
+// Types of datatypes
+// number => range is from 2 to power 53
+// bigint
+// string = ""
+// Boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof null)         // it will print the type is object
+console.log(typeof undefined)   // it will print the type is undefined
